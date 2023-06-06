4323a966516fb8db30df95daf724282acb06712c
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-06-07 00:14:12 +0600

    Server Connected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 
 
 function App() {
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3001/users')
+      .then(res => res.json())
+      .then(data => setUser(data))
+  }, [])
+  console.log(users)
   return (
     <div className="App">
       <header className="App-header">
-       <h2>Hi this is kawsar</h2>
+        {
+          users.map(user =>
+            <div>
+              <ul>
+                <li key={user.id}>Name:{user.name} Email: {user.email} Id: {user.id}</li>
+              </ul>
+            </div>
+          )
+        }
       </header>
     </div>
   );
