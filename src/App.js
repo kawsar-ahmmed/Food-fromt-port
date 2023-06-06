3b427e0e05038140973d9a8b4f90b93ec4c2c69a
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-06-07 03:09:22 +0600

    Create user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import './index.css'
 
 
 
@@ -9,15 +10,44 @@ function App() {
       .then(res => res.json())
       .then(data => setUser(data))
   }, [])
-  console.log(users)
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+
+    // POST data to the server
+    fetch('http://localhost:3001/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+  };
+
+
+
+
+
   return (
     <div className="App">
+      <form action="" onSubmit={handleAddUser}>
+        <input type="text" placeholder='name' name='name' />
+        <input type="email" placeholder='email' name='email' />
+        <button>Add user</button>
+      </form>
       <header className="App-header">
         {
           users.map(user =>
-            <div>
+            <div key={user.id}>
               <ul>
-                <li key={user.id}>Name:{user.name} Email: {user.email} Id: {user.id}</li>
+                <li>Name:{user.name} Email: {user.email} Id: {user.id}</li>
               </ul>
             </div>
           )
